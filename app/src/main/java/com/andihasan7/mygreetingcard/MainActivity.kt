7f5c6b5f9d23108c27f292da758c5dc3aa1f3b41
andihasan97 <andihasanelfalakiy@gmail.com>
2022-10-15 15:34:34 +0700

    tantangan google developer solved

--- app/src/main/java/com/andihasan7/mygreetingcard/MainActivity.kt
+++ app/src/main/java/com/andihasan7/mygreetingcard/MainActivity.kt
@@ -43,14 +43,14 @@ fun BirthdayGreetingWithText(message: String, from: String) {
             fontSize = 36.sp,
             modifier = Modifier
                 .fillMaxWidth()
-                .wrapContentWidth(Alignment.Start)
+                .wrapContentWidth(Alignment.CenterHorizontally)
                 .padding(start = 16.dp, top = 16.dp)
         )
         Text(text = from,
             fontSize = 24.sp,
             modifier = Modifier
                 .fillMaxWidth()
-                .wrapContentWidth(Alignment.End)
+                .wrapContentWidth(Alignment.CenterHorizontally)
                 .padding(start = 16.dp, end = 16.dp)
         )
     }
