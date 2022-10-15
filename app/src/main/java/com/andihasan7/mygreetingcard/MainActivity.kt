c9540c904f337b5866a95b859c25e6ac43bd13cf
andihasan97 <andihasanelfalakiy@gmail.com>
2022-10-15 15:16:51 +0700

    meratakan text & menambahkan padding

--- app/src/main/java/com/andihasan7/mygreetingcard/MainActivity.kt
+++ app/src/main/java/com/andihasan7/mygreetingcard/MainActivity.kt
@@ -9,10 +9,12 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.andihasan7.mygreetingcard.ui.theme.MyGreetingCardTheme
 
@@ -37,10 +39,18 @@ fun BirthdayGreetingWithText(message: String, from: String) {
     // column untuk tampilan secara vertical
     Column {
         Text(text = message,
-            fontSize = 36.sp
+            fontSize = 36.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.Start)
+                .padding(start = 16.dp, top = 16.dp)
         )
         Text(text = from,
-            fontSize = 24.sp
+            fontSize = 24.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.End)
+                .padding(start = 16.dp, end = 16.dp)
         )
     }
 }
